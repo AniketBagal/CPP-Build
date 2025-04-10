add8401c87701f5c6c47eb48ec4731a8ce17207a
AniketBagal <aniketbagal12345@gmail.com>
2025-04-10 15:25:05 +0530

    cpp build is updated

--- hello.cpp
+++ hello.cpp
@@ -3,13 +3,9 @@ using namespace std;
 
 int main()
 {
-	int a, b, c;
-	cout << "Hello World!!";
-	cout << "\n Enter 1st number :";
-	cin >> a;
-	cout << "\n Enter 2nd Number :";
-	cin >> b;
-	c = a + b;
-	cout << "\n Sum = " << c << endl;
+	
+	cout << "\nHii, I am Aniket Bagal\n" << endl;
+	cout << "Hello World!!" << endl;
+	
 	return 0;
 }
