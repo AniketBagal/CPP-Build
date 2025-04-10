339331e711db376e025d6e631f6b47bec30be1a4
AniketBagal <aniketbagal12345@gmail.com>
2025-04-10 15:11:14 +0530

    cpp build is updated

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main()
 {
+	int a, b, c;
 	cout << "Hello World!!";
+	cout << "\n Enter 1st number :";
+	cin >> a;
+	cout << "\n Enter 2nd Number :";
+	cin >> b;
+	c = a + b;
+	cout << "\n Sum = " << c << endl;
 	return 0;
 }
